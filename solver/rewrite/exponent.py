7e0acc73f6a3d50d79a9a84c0113ee2b12aceebb
Ignatius Foo <73582388+IGGYnatius0@users.noreply.github.com>
2026-06-29 06:01:54 +0000

    Added zero power rule (x^0=1)

--- solver/rewrite/exponent.py
+++ solver/rewrite/exponent.py
@@ -27,6 +27,10 @@ neg_pow = RewriteRule(
     form=a1**a2,
     target=one / a1**-a2
 )
+zero_pow = RewriteRule(
+    form=a1 ** zero,
+    target=one
+)
 exp_rules = RewriteGroup((
     pow_add, pow_sub, prod_exp, frac_exp, pow_exp, neg_pow
 ))
@@ -35,8 +39,8 @@ exp_rules = RewriteGroup((
 if __name__ == '__main__':
     from core.core_classes import *
     x = Var('x')
-    expr = x**-4
+    expr = (x**2+3/x-8*x)**0
     print(expr)
-    new = exp_rules.rewrite(expr, simplify=True)
+    new = exp_rules.rewrite(expr)
     for i in new:
         print(str(i))
