5037d3b5fc7f7e304faf74c0d69c820e60d3fbac
Ignatius <ignatiusfoo11@gmail.com>
2026-07-05 11:49:42 +0800

    Changed some RewriteRule to RewriteSet

--- solver/rewrite/exponent.py
+++ solver/rewrite/exponent.py
@@ -3,36 +3,40 @@ from forms.abc import *
 from core.num import *
 
 
-pow_add = RewriteRule(
-    form=a1**a2 * a1**a3,
-    target=a1 ** (a2+a3)
-)
-pow_sub = RewriteRule(
-    form=a1**a2 / a1**a3,
-    target=a1 ** (a2-a3)
-)
-prod_exp = RewriteRule(
-    form=a1**a3 * a2**a3,
-    target=(a1*a2) ** a3
-)
-frac_exp = RewriteRule(
-    form=a1**a3 / a2**a3,
-    target=(a1/a2) ** a3
-)
-pow_exp = RewriteRule(
-    form=(a1**a2) ** a3,
-    target=a1 ** (a2*a3)
-)
-neg_pow = RewriteRule(
+pow_add = RewriteSet((
+    a1**a2 * a1**a3,
+    a1 ** (a2+a3)
+))
+pow_sub = RewriteSet((
+    a1**a2 / a1**a3,
+    a1 ** (a2-a3)
+))
+prod_exp = RewriteSet((
+    a1**a3 * a2**a3,
+    (a1*a2) ** a3
+))
+frac_exp = RewriteSet((
+    a1**a3 / a2**a3,
+    (a1/a2) ** a3
+))
+pow_exp = RewriteSet((
+    (a1**a2) ** a3,
+    a1 ** (a2*a3)
+))
+neg_pow1 = RewriteRule(
     form=a1**a2,
     target=one / a1**-a2
+)
+neg_pow2 = RewriteRule(
+    form=one / a1**a2,
+    target=a1**-a2
 )
 zero_pow = RewriteRule(
     form=a1 ** zero,
     target=one
 )
 exp_rules = RewriteGroup((
-    pow_add, pow_sub, prod_exp, frac_exp, pow_exp, neg_pow
+    pow_add, pow_sub, prod_exp, frac_exp, pow_exp, neg_pow1, neg_pow2
 ))
 
 
