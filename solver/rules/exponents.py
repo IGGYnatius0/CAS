9a01648945a04580e87da8c333f4cde3d7327093
Ignatius <ignatiusfoo11@gmail.com>
2026-07-08 20:23:31 +0800

    Restructured rules

--- solver/rules/exponents.py
+++ solver/rules/exponents.py
@@ -3,40 +3,43 @@ from forms.abc import *
 from core.classes import zero, one
 
 
-pow_add = RewriteSet(( # TODO generalised version
-    a1**a2 * a1**a3,
-    a1 ** (a2+a3)
-))
-pow_sub = RewriteSet((
-    a1**a2 / a1**a3,
-    a1 ** (a2-a3)
-))
-prod_exp = RewriteSet(( # TODO generalised version
-    a1**a3 * a2**a3,
-    (a1*a2) ** a3
-))
-frac_exp = RewriteSet((
-    a1**a3 / a2**a3,
-    (a1/a2) ** a3
-))
-pow_exp = RewriteSet((
-    (a1**a2) ** a3,
-    a1 ** (a2*a3)
-))
-neg_pow1 = RewriteRule(
-    form=a1**a2,
-    target=one / a1**-a2
-)
-neg_pow2 = RewriteRule(
-    form=one / a1**a2,
-    target=a1**-a2
-)
-zero_pow = RewriteRule(
-    form=a1 ** zero,
-    target=one
-)
 rules = RewriteGroup((
-    pow_add, pow_sub, prod_exp, frac_exp, pow_exp, neg_pow1, neg_pow2, zero_pow
+    RewriteSet(( # TODO generalised version
+        a1**a2 * a1**a3,
+        a1 ** (a2+a3)
+    )),
+    RewriteSet((
+        a1**a2 / a1**a3,
+        a1 ** (a2-a3)
+    )),
+    RewriteSet(( # TODO generalised version
+        a1**a3 * a2**a3,
+        (a1*a2) ** a3
+    )),
+    RewriteSet((
+        a1**a3 / a2**a3,
+        (a1/a2) ** a3
+    )),
+    RewriteSet((
+        (a1**a2) ** a3,
+        a1 ** (a2*a3)
+    )),
+    RewriteRule(
+        form=a1**a2,
+        target=one / a1**-a2
+    ),
+    RewriteRule(
+        form=one / a1**a2,
+        target=a1**-a2
+    ),
+    RewriteRule(
+        form=a1 ** zero,
+        target=one
+    ),
+    RewriteSet((
+        a1**one,
+        a1
+    ))
 ))
 
 
