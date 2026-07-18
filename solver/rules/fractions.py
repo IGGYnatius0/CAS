63e6880ae0a7463501d9887a73d0450687823b54
Ignatius <ignatiusfoo11@gmail.com>
2026-07-18 11:28:01 +0800

    Added general cases

--- solver/rules/fractions.py
+++ solver/rules/fractions.py
@@ -1,6 +1,52 @@
 from solver.rules.base import *
 from forms.abc import *
-from core.classes import Var, one
+from core.classes import *
+
+
+def prod_fracs_rewrite(expr, simplify=True):
+    if not isinstance(expr, Prod):
+        return []
+    if len(expr.factors) < 2:
+        return []
+    for factor in expr.factors:
+        if not isinstance(factor, Frac):
+            return []
+    numer = Prod([factor.numer for factor in expr.factors])
+    denom = Prod([factor.denom for factor in expr.factors])
+    output = Frac(numer, denom)
+    if simplify:
+        output.simplify()
+    return [output]
+
+
+def sum_fracs_rewrite(expr, simplify=True):
+    if not isinstance(expr, Sum):
+        return []
+    if len(expr.terms) < 2:
+        return []
+    for term in expr.terms:
+        if not isinstance(term, Frac):
+            return []
+    numer = []
+    for i in range(len(expr.terms)):
+        factors = []
+        for j, term in enumerate(expr.terms):
+            if i == j:
+                factors.append(term.numer)
+            else:
+                factors.append(term.denom)
+        numer.append(Prod(factors))
+    denom = Prod([term.denom for term in expr.terms])
+    output = Frac(Sum(numer), denom)
+    if simplify:
+        output.simplify()
+    return [output]
+
+
+prod_fracs = RewriteRule()
+sum_fracs = RewriteRule()
+prod_fracs.rewrite = prod_fracs_rewrite
+sum_fracs.rewrite = sum_fracs_rewrite
 
 
 rules = RewriteGroup((
@@ -20,14 +66,11 @@ rules = RewriteGroup((
         one/(a1/a2),
         a2/a1
     )),
-    RewriteRule( # TODO generalised version
-        form=(a1/a2)*(a3/a4),
-        target=(a1*a3)/(a2*a4)
-    ),
-    RewriteRule( # TODO generalised version
-        form=(a1/a2)+(a3/a4),
-        target=(a1*a3+a2*a3)/(a2*a4)
-    ),
+
+    prod_fracs,
+
+    sum_fracs,
+
     RewriteSet((
         (a1/a2)/a3,
         (a1/a2)*(one/a3)
@@ -43,9 +86,9 @@ rules = RewriteGroup((
 ))
 
 if __name__ == '__main__':
-    x = Var('x')
-    expr = 6 - 17 / x + 12 / (x ** 2)
+    from core.abc import a, b, c, d, e, f, g, h
+    expr = (a/b)*(c/d)*(e/f)*(g/h)
     print(expr)
-    new = rules.rewrite(expr)
+    new = prod_fracs_rewrite(expr)
     for i in new:
         print(i)
