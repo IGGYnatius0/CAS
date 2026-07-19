980b6f8532ecded22c82dec33993d6d21b0dc2da
Ignatius <ignatiusfoo11@gmail.com>
2026-07-19 11:17:58 +0800

    Added rewrite

--- solver/rewrite/__init__.py
+++ solver/rewrite/__init__.py
@@ -1,9 +1,13 @@
+from core.classes import *
 from solver.rewrite.expr import rewrite as expr_rewrite
 from solver.rewrite.eqn import rewrite as eqn_rewrite
 
 
-__all__ = ['rewrite']
-
-
 def rewrite(eqn):
-    pass
+    new_eqns = []
+    new_eqns.extend(eqn_rewrite(eqn))
+    new_lhs = expr_rewrite(eqn.lhs)
+    new_rhs = expr_rewrite(eqn.rhs)
+    new_eqns.extend([Eqn(lhs, eqn.rhs) for lhs in new_lhs])
+    new_eqns.extend([Eqn(eqn.lhs, rhs) for rhs in new_rhs])
+    return list(dict.fromkeys(new_eqns))
