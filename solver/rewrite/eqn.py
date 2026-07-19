665f9838a71df8c33d396acb1745c0d9f3456117
Ignatius <ignatiusfoo11@gmail.com>
2026-07-19 10:09:52 +0800

    Fixed arrange_terms and arrange_factors

--- solver/rewrite/eqn.py
+++ solver/rewrite/eqn.py
@@ -1,4 +1,5 @@
 from itertools import chain, combinations
+
 from core.classes import *
 
 
@@ -19,6 +20,22 @@ def powerset(iterable):
     return chain.from_iterable(combinations(iterable, r) for r in range(len(iterable) + 1))
 
 
+def clean_terms(terms):
+    if len(terms) == 0:
+        return zero
+    if len(terms) == 1:
+        return terms[0]
+    return Sum(terms)
+
+
+def clean_factors(factors):
+    if len(factors) == 0:
+        return one
+    if len(factors) == 1:
+        return factors[0]
+    return Prod(factors)
+
+
 def arrange_terms(eqn: Eqn):
     terms = []
     if isinstance(eqn.lhs, Sum):
@@ -26,11 +43,10 @@ def arrange_terms(eqn: Eqn):
     elif eqn.lhs != zero:
         terms.append(eqn.lhs)
     if isinstance(eqn.rhs, Sum):
-        terms.extend(eqn.rhs.terms)
+        terms.extend([-term for term in eqn.rhs.terms])
     elif eqn.rhs != zero:
-        terms.append(eqn.rhs)
+        terms.append(-eqn.rhs)
     idxs = powerset(range(len(terms) - 1))
-    next(idxs)
     new_eqns = []
     for idx in idxs:
         lhs, rhs = [], []
@@ -39,18 +55,8 @@ def arrange_terms(eqn: Eqn):
                 rhs.append((-term).simplify())
             else:
                 lhs.append(term)
-        if len(lhs) == 0:
-            sum_lhs = zero
-        elif len(lhs) == 1:
-            sum_lhs = lhs[0]
-        else:
-            sum_lhs = Sum(lhs)
-        if len(rhs) == 0:
-            sum_rhs = zero
-        elif len(rhs) == 1:
-            sum_rhs = rhs[0]
-        else:
-            sum_rhs = Sum(rhs)
+        sum_lhs = clean_terms(lhs)
+        sum_rhs = clean_terms(rhs)
         new_eqns.append(Eqn(sum_lhs, sum_rhs))
     return new_eqns
 
@@ -62,22 +68,25 @@ def invert_factor(factor):
         return Frac(factor.denom, factor.numer)
     if isinstance(factor, Exp):
         return Exp(factor.base, (-factor.power).simplify())
-    return one / factor
+    return Exp(factor, neg_one)
+
+
+def soft_decomp(expr):
+    if isinstance(expr, Prod):
+        return expr.factors
+    if isinstance(expr, Frac):
+        numer = soft_decomp(expr.numer)
+        denom = [invert_factor(factor) for factor in soft_decomp(expr.denom)]
+        return numer + denom
+    return [expr]
 
 
 # TODO denom != 0
 def arrange_factors(eqn: Eqn):
     factors = []
-    if isinstance(eqn.lhs, Prod):
-        factors.extend(eqn.lhs.factors)
-    elif eqn.lhs != one:
-        factors.append(eqn.lhs)
-    if isinstance(eqn.rhs, Prod):
-        factors.extend(eqn.rhs.factors)
-    elif eqn.rhs != one:
-        factors.append(eqn.rhs)
+    factors.extend(soft_decomp(eqn.lhs))
+    factors.extend([invert_factor(factor) for factor in soft_decomp(eqn.rhs)])
     idxs = powerset(range(len(factors) - 1))
-    next(idxs)
     new_eqns = []
     for idx in idxs:
         lhs, rhs = [], []
@@ -86,27 +95,17 @@ def arrange_factors(eqn: Eqn):
                 rhs.append(invert_factor(factor))
             else:
                 lhs.append(factor)
-        if len(lhs) == 0:
-            prod_lhs = zero
-        elif len(lhs) == 1:
-            prod_lhs = lhs[0]
-        else:
-            prod_lhs = Prod(lhs)
-        if len(rhs) == 0:
-            prod_rhs = zero
-        elif len(rhs) == 1:
-            prod_rhs = rhs[0]
-        else:
-            prod_rhs = Prod(rhs)
+        prod_lhs = clean_factors(lhs)
+        prod_rhs = clean_factors(rhs)
         new_eqns.append(Eqn(prod_lhs, prod_rhs))
     return new_eqns
 
 
 if __name__ == '__main__':
     x = Var('x')
-    eqn = Eqn(x**2 - 3*x + 14, zero).simplify()
+    eqn = Eqn(x*(x+1)/(x+2)*(x+3)**3, 1/(x+4)).simplify()
     print(eqn)
-    new = arrange_terms(eqn)
+    new = arrange_factors(eqn)
     print(len(new))
     for i in new:
         print(i)
