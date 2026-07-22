b3ffb7c4297eafd1d4525842075aef66af4cf102
Ignatius <ignatiusfoo11@gmail.com>
2026-07-22 16:51:15 +0800

    Changed rules to be Eqn based

--- solve_rules/polynomial.py
+++ solve_rules/polynomial.py
@@ -1,4 +1,4 @@
-from forms.classes import FormConst
+from forms.classes import FormConst, FormEqn
 from forms.abc import *
 from solve_rules.base import *
 
@@ -8,11 +8,11 @@ A = FormConst('A', '(0, inf)')
 
 rules = SolveGroup((
     SolveRule(
-        target_form=A*x+b,
+        target_form=FormEqn(A*x+B, 0),
         formula=-B/A
     ),
     SolveRule(
-        target_form=A*x**2+B*x+C,
+        target_form=FormEqn(A*x**2+B*x+C, 0),
         formula=((-B+(B**2-4*A*C)**0.5)/(2*A),
                  (-B-(B**2-4*A*C)**0.5)/(2*A))
     ),
