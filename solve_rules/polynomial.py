a2a1c4623738a31d6788c8aaff218e6e9a46125b
Ignatius <ignatiusfoo11@gmail.com>
2026-07-22 11:54:10 +0800

    Added polynomial forms

---
+++ solve_rules/polynomial.py
@@ -0,0 +1,25 @@
+from forms.classes import FormConst
+from forms.abc import *
+from known_forms.base import *
+
+
+A = FormConst('A', '(0, inf)')
+
+
+rules = SolveGroup((
+    SolveRule(
+        target_form=A*x+b,
+        formula=-B/A
+    ),
+    SolveRule(
+        target_form=A*x**2+B*x+C,
+        formula=((-B+(B**2-4*A*C)**0.5)/(2*A),
+                 (-B-(B**2-4*A*C)**0.5)/(2*A))
+    ),
+
+    # cubic,
+    #
+    # quartic,
+    #
+    # generalised
+))
