2252d1c1edcc88ecafe300df77ad27be439f82a9
Ignatius <ignatiusfoo11@gmail.com>
2026-07-22 16:50:43 +0800

    Added auto converting to Eqn

--- solve_rules/base.py
+++ solve_rules/base.py
@@ -1,4 +1,4 @@
-from forms.classes import FORM_TYPES
+from forms.classes import FORM_TYPES, FormEqn
 import forms.matcher as matcher
 
 
@@ -8,12 +8,15 @@ __all__ = ['SolveRule', 'SolveGroup']
 class SolveRule:
     def __init__(self, target_form, formula):
         if target_form is not None:
-            self.target_form = target_form.group_consts()
+            if isinstance(target_form, FORM_TYPES) and not isinstance(target_form, FormEqn):
+                self.target_form = FormEqn(target_form, 0).group_consts()
+            else:
+                self.target_form = target_form.group_consts()
             if isinstance(formula, FORM_TYPES):
                 formula = (formula,)
             formula = tuple(formula)
             for formula_ in formula:
-                if formula_.get_vars():
+                if formula_.get_vars:
                     raise ValueError("'formulas' cannot contain FormVars")
             self.formula = tuple(formula)
 
