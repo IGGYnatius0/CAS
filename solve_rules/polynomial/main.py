872c13eb384fc299f925fe68960d2e9a2ac41a2f
Ignatius <ignatiusfoo11@gmail.com>
2026-07-23 20:18:07 +0800

    Changing folder structure

---
+++ solve_rules/polynomial/main.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+
+from core.classes import *
+from forms.matcher import match
+from forms.abc import *
+
+
+def is_poly_expr(expr):
+    if len(expr.get_vars) != 1:
+        return False
+    temp = defaultdict(int)
+    if isinstance(expr, Sum):
+        for term in expr.terms:
+            if isinstance(term, Num):
+                temp[zero] = term
+                continue
+            result = match(A * x ** B, term)
+            if not result:
+                return False
+            temp[result['consts'][B]] = result['consts'][A]
+    coeffs = [zero] * int(max(temp.keys()) + 1)
+    for i, coeff in temp.items():
+        coeffs[int(i)] = coeff
+    return coeffs[::-1]
+
+
+def rewrite(eqn):
+    pass
+
+
+if __name__ == '__main__':
+    y = Var('x')
+    print(is_poly_expr((Num(10)).simplify()))
--- solver/search.py
+++ solver/search.py
@@ -3,15 +3,30 @@ from rewrite import rewrite
 from solve_rules import RULES
 
 
-def iterative_deepening_inner(eqn, max_depth, current_depth):
+def iterative_deepening_inner(eqn, seen, max_depth, current_depth):
     solns = RULES.apply(eqn)
     if solns:
         return solns
     if current_depth == max_depth:
         return None
     new_eqns = rewrite(eqn)
+    for i, new_eqn in reversed(tuple(enumerate(new_eqns))):
+        if new_eqn in seen:
+            new_eqns.pop(i)
+    seen.update(new_eqns)
     for new_eqn in new_eqns:
-        solns = iterative_deepening_inner(new_eqn, max_depth, current_depth + 1)
+        solns = iterative_deepening_inner(new_eqn, seen, max_depth, current_depth + 1)
+        if solns:
+            return solns
+    return None
+
+
+def iterative_deepening(eqn, max_depth=6):
+    for i in range(1, max_depth + 1):
+        print(f'depth {i}')
+        seen = set()
+        solns = iterative_deepening_inner(eqn, seen, i, 0)
+        print(len(seen))
         if solns:
             return solns
     return None
@@ -19,5 +34,7 @@ def iterative_deepening_inner(eqn, max_depth, current_depth):
 
 if __name__ == '__main__':
     x = Var('x')
-    eqn = Eqn(x+1, 0)
-    print(iterative_deepening_inner(eqn, 1, 0))
+    eqn = Eqn(2*x+3, x-5)
+    solns = iterative_deepening(eqn)
+    for soln in solns:
+        print(soln)
