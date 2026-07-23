fbd33d42df06b7aa5d6c0d47c51751f5c266c996
Ignatius <ignatiusfoo11@gmail.com>
2026-07-23 20:28:02 +0800

    Changing folder structure

---
+++ solve_rules/polynomial/forms.py
@@ -0,0 +1,18 @@
+from forms.classes import FormEqn
+from forms.abc import *
+from solve_rules.base import *
+
+
+rules = SolveGroup((
+    SolveRule(
+        target_form=FormEqn(A*x+B, 0),
+        formula=-B/A
+    ),
+
+    SolveRule(
+        target_form=FormEqn(A*x**2+B*x+C, 0),
+        formula=((-B+(B**2-4*A*C)**0.5)/(2*A),
+                 (-B-(B**2-4*A*C)**0.5)/(2*A))
+    )
+))
+
