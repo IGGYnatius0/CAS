fd19c4c9cbe67b0721c81843497db8fe1c76c890
Ignatius <ignatiusfoo11@gmail.com>
2026-07-23 20:33:02 +0800

    Removed coeff finding code

--- solver/polynomial/main.py
+++ solver/polynomial/main.py
@@ -8,24 +8,21 @@ from forms.abc import *
 def is_poly_expr(expr):
     if len(expr.get_vars) != 1:
         return False
-    temp = defaultdict(int)
+    # temp = defaultdict(int)
     if isinstance(expr, Sum):
         for term in expr.terms:
             if isinstance(term, Num):
-                temp[zero] = term
+                # temp[zero] = term
                 continue
             result = match(A * x ** B, term)
             if not result:
                 return False
-            temp[result['consts'][B]] = result['consts'][A]
-    coeffs = [zero] * int(max(temp.keys()) + 1)
-    for i, coeff in temp.items():
-        coeffs[int(i)] = coeff
-    return coeffs[::-1]
-
-
-def rewrite(eqn):
-    pass
+            # temp[result['consts'][B]] = result['consts'][A]
+    # coeffs = [zero] * int(max(temp.keys()) + 1)
+    # for i, coeff in temp.items():
+    #     coeffs[int(i)] = coeff
+    # return coeffs[::-1]
+    return True
 
 
 if __name__ == '__main__':
