cd6303245069656d1c0ec71697dd33af73ccf4e4
Ignatius <ignatiusfoo11@gmail.com>
2026-07-23 21:16:39 +0800

    Main solve logic

--- solver/main.py
+++ solver/main.py
@@ -1,11 +1,26 @@
 from core.classes import *
+from solver.polynomial import solve as poly_solve
 
 
-def solve(expr):
-    pass
+solvers = (
+    poly_solve,
+)
+
+
+def solve(expr): # TODO use SolveGroup?
+    if isinstance(expr, CORE_TYPES) and not isinstance(expr, Eqn):
+        eqn = Eqn(expr, zero)
+    else:
+        eqn = expr
+    eqn = eqn.simplify()
+    for solver in solvers:
+        result = solver(eqn)
+        if result:
+            return result
+    return []
 
 
 if __name__ == '__main__':
     x = Var('x')
-    expr = 6 - 17/x + 12/(x**2)
+    expr = 6 - 17*x + 12*(x**2)
     print(solve(expr))
